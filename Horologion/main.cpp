d4f354eaacda2f9e296b55cbe0396c48ad3bfcf8
David Weber <dsw7@sfu.ca>
2022-09-03 03:21:45 -0700

    Add basic logger

--- Horologion/main.cpp
+++ Horologion/main.cpp
@@ -1,17 +1,52 @@
 #include <iostream>
 #include <errno.h>
 #include <string.h>
+#include <time.h>
+#include <string>
 #include <linux/reboot.h>  // For LINUX_REBOOT_CMD_POWER_OFF
 
 // System calls
 #include <unistd.h>        // For sync(2)
 #include <sys/reboot.h>    // For reboot(2)
 
+std::string get_current_datetime_string()
+{
+    time_t time_now = time(0);
+    struct tm time_struct;
+    char   time_buffer[80];
+
+    time_struct = *localtime(&time_now);
+    strftime(time_buffer, sizeof(time_buffer), "%Y-%m-%dT%X", &time_struct);
+    return time_buffer;
+}
+
+namespace Logger {
+
+    void info(const std::string &message)
+    {
+        std::string line = get_current_datetime_string() + " I " + message;
+        std::cout << line << std::endl;
+    }
+
+    void warning(const std::string &message)
+    {
+        std::string line = get_current_datetime_string() + " W " + message;
+        std::cout << line << std::endl;
+    }
+
+    void error(const std::string &message)
+    {
+        std::string line = get_current_datetime_string() + " E " + message;
+        std::cout << line << std::endl;
+    }
+
+}
+
 bool is_running_as_root()
 {
     if (getuid() != 0)
     {
-        std::cerr << "Not running as root. Additional privileges needed!" << std::endl;
+        Logger::error("Not running as root. Additional privileges needed!");
         return false;
     }
 
