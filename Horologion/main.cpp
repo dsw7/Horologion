cd714b808d6354380bdd9a7d849ef1d83745bb0a
David Weber <dsw7@sfu.ca>
2022-09-03 03:06:48 -0700

    Add basic skeleton

--- Horologion/main.cpp
+++ Horologion/main.cpp
@@ -1,4 +1,31 @@
+#include <iostream>
+#include <errno.h>
+#include <string.h>
+#include <linux/reboot.h>  // For LINUX_REBOOT_CMD_POWER_OFF
+
+// System calls
+#include <unistd.h>        // For sync(2)
+#include <sys/reboot.h>    // For reboot(2)
+
+bool shutdown()
+{
+    sync();
+
+    if (reboot(LINUX_REBOOT_CMD_POWER_OFF) != 0)
+    {
+        std::cerr << strerror(errno) << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
 int main()
 {
+    if (not shutdown())
+    {
+        return EXIT_FAILURE;
+    }
+
     return 0;
 }
