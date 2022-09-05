ea670ea39e9eede3851c2a8654879e6a97d245ba
David Weber <dsw7@sfu.ca>
2022-09-05 03:21:12 -0700

    Move CLI stuff to separate file

--- Horologion/src/main.cpp
+++ Horologion/src/main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <string>
 
 #include <errno.h>
@@ -9,6 +8,7 @@
 #include <unistd.h>        // For sync(2)
 #include <sys/reboot.h>    // For reboot(2)
 
+#include "command_line_interface.h"
 #include "logger.h"
 #include "file_utils.h"
 #include "time_utils.h"
@@ -71,22 +71,6 @@ bool shutdown(bool is_dry_run)
     return true;
 }
 
-void help_commands(char *filename)
-{
-    std::cerr << "\e[1m\e[4mUsage:\e[0m:\n\n";
-    std::cerr << "  $ " << filename << " <command> [<options>]\n\n";
-
-    std::string commands =
-    "\e[1m\e[4mCommands\e[0m:\n\n"
-    "  -h, --help  Print help information and exit.\n"
-    "  set-alarm   \e[1mSet alarm clock.\e[0m This command will write out an Epoch time to the system's"
-    " wakealarm file.\n"
-    "  trigger     \e[1mTrigger a wake-sleep cycle.\e[0m This command should be invoked by systemd or an"
-    " equivalent init system.\n";
-
-    std::cerr << commands << std::endl;
-}
-
 int main(int argc, char **argv)
 {
     if (argc < 2)
@@ -94,10 +78,6 @@ int main(int argc, char **argv)
         help_commands(argv[0]);
         return EXIT_FAILURE;
     }
-
-    std::cout << argv[0] << std::endl;
-    argv++;
-    std::cout << argv[0] << std::endl;
 
     if (not is_running_as_root())
     {
