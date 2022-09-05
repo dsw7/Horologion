4ebc1b28486f040a8558ad73d25e14ae7616a713
David Weber <dsw7@sfu.ca>
2022-09-05 03:42:28 -0700

    Add CLI to separate file

---
+++ Horologion/src/command_line_interface.cpp
@@ -0,0 +1,17 @@
+#include "command_line_interface.h"
+
+void help_commands(char *filename)
+{
+    std::cerr << "\e[1m\e[4mUsage:\e[0m:\n\n";
+    std::cerr << "  $ " << filename << " <command> [<options>]\n\n";
+
+    std::string commands =
+    "\e[1m\e[4mCommands\e[0m:\n\n"
+    "  -h, --help  Print help information and exit.\n"
+    "  set-alarm   \e[1mSet alarm clock.\e[0m This command will write out an Epoch time to the system's"
+    " wakealarm file.\n"
+    "  trigger     \e[1mTrigger a wake-sleep cycle.\e[0m This command should be invoked by systemd or an"
+    " equivalent init system.\n";
+
+    std::cerr << commands << std::endl;
+}
---
+++ Horologion/src/command_line_interface.h
@@ -0,0 +1,8 @@
+#ifndef COMMAND_LINE_INTERFACE_H
+#define COMMAND_LINE_INTERFACE_H
+
+#include <iostream>
+
+void help_commands(char *filename);
+
+#endif
