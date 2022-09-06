431fd2e7ebe2e57b37b43f1b3932e6eb91dbc796
David Weber <dsw7@sfu.ca>
2022-09-05 22:12:48 -0700

    Setup reset command

--- Horologion/src/commands.cpp
+++ Horologion/src/commands.cpp
@@ -11,6 +11,24 @@ bool CommandBase::is_running_as_root()
     return true;
 }
 
+bool CommandBase::wakealarm_exists()
+{
+    Logger::info("Checking if wakealarm file exists: " + SYSFS_WAKEALARM);
+
+    if (not file_exists(SYSFS_WAKEALARM))
+    {
+        return false;
+    }
+}
+
+void CommandBase::reset_rtc_alarm()
+{
+    Logger::info("Resetting alarm");
+
+    std::string reset_str = "0";
+    write_to_file(SYSFS_WAKEALARM, reset_str);
+}
+
 bool CommandResetAlarm::main()
 {
     if (not this->is_running_as_root())
@@ -18,12 +36,23 @@ bool CommandResetAlarm::main()
         return false;
     }
 
+    if (not this->wakealarm_exists())
+    {
+        return false;
+    }
+
+    reset_rtc_alarm();
     return true;
 };
 
 bool CommandSetAlarm::main()
 {
     if (not this->is_running_as_root())
+    {
+        return false;
+    }
+
+    if (not this->wakealarm_exists())
     {
         return false;
     }
--- Horologion/src/commands.h
+++ Horologion/src/commands.h
@@ -2,12 +2,18 @@
 #define COMMANDS_H
 
 #include <unistd.h>
+
 #include "logger.h"
+#include "utils_file.h"
+
+const std::string SYSFS_WAKEALARM = "/sys/class/rtc/rtc0/wakealarm";
 
 class CommandBase
 {
     protected:
         bool is_running_as_root();
+        bool wakealarm_exists();
+        void reset_rtc_alarm();
 
     public:
         virtual bool main() {};
