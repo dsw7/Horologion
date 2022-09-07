38cc03156efd3125e3478bb4c5d68cf343571053
David Weber <dsw7@sfu.ca>
2022-09-06 23:09:04 -0700

    Add more time utils

--- Horologion/src/utils_time.cpp
+++ Horologion/src/utils_time.cpp
@@ -1,26 +1,26 @@
 #include "utils_time.h"
 
+std::string epoch_time_to_ascii_time(std::time_t &epoch_time)
+{
+    return std::string(std::asctime(std::localtime(&epoch_time)));
+}
+
 std::time_t compute_delay(std::time_t offset_seconds)
 {
     std::time_t time_since_epoch = std::time(nullptr);
 
     Logger::info("The current time is: ");
-    Logger::info(std::asctime(std::localtime(&time_since_epoch)));
+    epoch_time_to_ascii_time(time_since_epoch);
 
     std::time_t offset_time = time_since_epoch + offset_seconds;
 
     Logger::info("The offset time was computed as: ");
-    Logger::info(std::asctime(std::localtime(&offset_time)));
+    epoch_time_to_ascii_time(offset_time);
 
     return offset_time;
 }
 
-std::time_t get_epoch_time_from_tm(tm &time)
+std::time_t get_epoch_time_from_tm(tm &tm_time)
 {
-    std::time_t epoch_time = mktime(&time);
-
-    Logger::info("Parsed time: ");
-    Logger::info(std::asctime(std::localtime(&epoch_time)));
-
-    return epoch_time;
+    return mktime(&tm_time);
 }
--- Horologion/src/utils_time.h
+++ Horologion/src/utils_time.h
@@ -1,10 +1,12 @@
 #ifndef UTILS_TIME_H
 #define UTILS_TIME_H
 
+#include <string>
 #include <ctime>
 #include <time.h>
 #include "logger.h"
 
+std::string epoch_time_to_ascii_time(std::time_t &epoch_time);
 std::time_t compute_delay(std::time_t offset_seconds);
 std::time_t get_epoch_time_from_tm(tm &time);
 
