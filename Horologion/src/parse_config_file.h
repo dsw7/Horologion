431d8815ef94bad8e1bbd8219c9b51ccfc5a0812
David Weber <dsw7@sfu.ca>
2022-09-06 21:32:09 -0700

    Add config file parser

--- Horologion/src/parse_config_file.h
+++ Horologion/src/parse_config_file.h
@@ -2,14 +2,9 @@
 #define PARSE_CONFIG_FILE_H
 
 #include <string>
-#include <iostream>
 #include <map>
 #include <sstream>
-#include <fstream>
-#include "configs.h"
-#include "utilities.h"
 
-bool read_configs_from_file(std::string path_config_file, Configs &configs);
 void parse_configs(std::string &file_contents, std::map<std::string, std::string> &raw_configs);
 
 #endif
