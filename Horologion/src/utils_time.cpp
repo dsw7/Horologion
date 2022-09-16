a5af90eecea1df5624671c658b8de4138b01073a
David Weber <dsw7@sfu.ca>
2022-09-16 01:54:23 -0700

    Remove unused time function

--- Horologion/src/utils_time.cpp
+++ Horologion/src/utils_time.cpp
@@ -5,21 +5,6 @@ std::string epoch_time_to_ascii_time(std::time_t &epoch_time)
     return std::string(std::asctime(std::localtime(&epoch_time)));
 }
 
-std::time_t compute_delay(std::time_t offset_seconds)
-{
-    std::time_t time_since_epoch = std::time(nullptr);
-
-    Logger::info("The current time is: ");
-    epoch_time_to_ascii_time(time_since_epoch);
-
-    std::time_t offset_time = time_since_epoch + offset_seconds;
-
-    Logger::info("The offset time was computed as: ");
-    epoch_time_to_ascii_time(offset_time);
-
-    return offset_time;
-}
-
 std::time_t get_epoch_time_from_tm(tm &tm_time)
 {
     return mktime(&tm_time);
--- Horologion/src/utils_time.h
+++ Horologion/src/utils_time.h
@@ -7,7 +7,6 @@
 #include "logger.h"
 
 std::string epoch_time_to_ascii_time(std::time_t &epoch_time);
-std::time_t compute_delay(std::time_t offset_seconds);
 std::time_t get_epoch_time_from_tm(tm &time);
 
 #endif
