f20efe8b4fb7008804b9e27e4a0b37112ce8d917
David Weber <dsw7@sfu.ca>
2022-09-19 00:39:25 -0700

    Add default bin + log paths

--- Horologion/src/command_base.h
+++ Horologion/src/command_base.h
@@ -23,8 +23,12 @@ struct Configs
 {
     std::vector<std::pair<std::string, std::string>> commands = {};
     std::string suspend_type = "reboot";
+
     struct tm time_alarm = {0};
     struct tm time_sleep = {0};
+
+    std::string path_to_binary = "/usr/bin/horolog";
+    std::string path_to_log = "/var/log/horolog.log";
 };
 
 class CommandBase
