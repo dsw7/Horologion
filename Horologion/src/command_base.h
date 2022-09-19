0e3eef91282cddf545fbe7dc121bbc6fe695a1a8
David Weber <dsw7@sfu.ca>
2022-09-18 21:52:23 -0700

    Add default crontab

--- Horologion/src/command_base.h
+++ Horologion/src/command_base.h
@@ -16,6 +16,7 @@
 
 const std::string SYSFS_WAKEALARM = "/sys/class/rtc/rtc0/wakealarm";
 const std::string PROG_CONFIG = "/etc/horolog.ini";
+const std::string ETC_CRONTAB = "/etc/cron.d/horolog";
 const std::time_t MINIMUM_WAKE_TIME = 60;
 
 struct Configs
