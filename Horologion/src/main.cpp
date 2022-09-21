ef9c053dd0596100d347bbc03bad4f2e9fcb43c2
David Weber <dsw7@sfu.ca>
2022-09-21 01:43:13 -0700

    Fix help menu

--- Horologion/src/main.cpp
+++ Horologion/src/main.cpp
@@ -7,18 +7,20 @@
 
 void help_commands(char *filename)
 {
-    std::cerr << "\e[1m\e[4mUsage:\e[0m:\n\n";
+    std::cerr << "\e[1m\e[4mUsage:\e[0m\n\n";
     std::cerr << "  $ " << filename << " <command> [<options>]\n\n";
 
     std::string commands =
-    "\e[1m\e[4mCommands\e[0m:\n\n"
+    "\e[1m\e[4mCommands:\e[0m\n\n"
     "  -h, --help    Print help information and exit.\n"
-    "  enable        \e[1mSet alarm clock.\e[0m This command will write out an Epoch time to the system's"
-    " wakealarm file.\n"
-    "  disable       \e[1mReset alarm clock.\e[0m This command will erase the contents of the system's"
-    " wakealarm file.\n"
-    "  trigger       \e[1mTrigger a wake-sleep cycle.\e[0m This command should be only be run manually for"
-    " troubleshooting purposes.\n";
+    "  enable        \e[1mEnable daily wake-sleep cycle.\e[0m\n"
+    "                  > This command will write out an Epoch time to the system's wakealarm file.\n"
+    "                  > This command will enable a daily horolog trigger cron job.\n"
+    "  disable       \e[1mDisable daily wake-sleep cycle.\e[0m\n"
+    "                  > This command will erase the contents of the system's wakealarm file.\n"
+    "                  > This command will disable a daily horolog trigger cron job.\n"
+    "  trigger       \e[1mTrigger a wake-sleep cycle.\e[0m\n"
+    "                  > This command should be only be run manually for troubleshooting purposes.\n";
 
     std::cerr << commands << std::endl;
 }
