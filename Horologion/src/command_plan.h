59c51a5cf38142a8fb956d467837398c983309f5
David Weber <dsw7@sfu.ca>
2022-09-24 06:05:38 -0700

    Remove unused const defines

--- Horologion/src/command_plan.h
+++ Horologion/src/command_plan.h
@@ -11,15 +11,9 @@
 const std::string V_LINE = "\u2502";
 const std::string H_LINE = "\u2500";
 const std::string RIGHT_TEE = "\u251c";
-const std::string LEFT_TEE = "\u2524";
 const std::string DOWN_TEE = "\u252c";
-const std::string UP_TEE = "\u2534";
-const std::string INTERSEC = "\u253c";
 const std::string BL_ELBOW = "\u2514";
 const std::string TL_ELBOW = "\u250c";
-const std::string TR_ELBOW = "\u2510";
-const std::string BR_ELBOW = "\u2518";
-const std::string SQUARE = "\u25a0";
 
 class CommandPlan: public CommandBase
 {
