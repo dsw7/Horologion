04601888af0a3f77030f37409fc74fc06d7da136
dsw7 <dds3443@hotmail.com>
2025-05-10 02:17:22 -0700

    Remove manual config file parsing

--- Horologion/src/configs.cpp
+++ Horologion/src/configs.cpp
@@ -3,66 +3,28 @@
 #include "logger.hpp"
 #include "utils.hpp"
 
-#include <map>
-#include <sstream>
 #include <stdexcept>
+#include <toml.hpp>
 
 namespace {
 
-const std::string PROG_CONFIG = "/etc/horolog.ini";
-
-void strip_whitespace_from_left(std::string &str)
+void read_project_toml(Configs &configs)
 {
-    const std::size_t found_ws = str.find_first_not_of(" ");
+    static std::string prog_config = "/etc/horolog.toml";
+    toml::table table = toml::parse_file(prog_config);
 
-    if (found_ws == std::string::npos) {
-        return;
-    }
+    configs.time_wake.tm_hour = table["times"]["wake"]["hour"].value_or<int>(8);
+    configs.time_wake.tm_min = table["times"]["wake"]["minute"].value_or<int>(0);
+    configs.time_run_cmd.tm_hour = table["times"]["cmd"]["hour"].value_or<int>(8);
+    configs.time_run_cmd.tm_min = table["times"]["cmd"]["minute"].value_or<int>(1);
+    configs.time_sleep.tm_hour = table["times"]["sleep"]["hour"].value_or<int>(8);
+    configs.time_sleep.tm_min = table["times"]["sleep"]["minute"].value_or<int>(2);
+    configs.suspend_type = table["suspend-type"].value_or<std::string>("mem");
 
-    str.erase(0, found_ws);
-}
-
-void strip_whitespace_from_right(std::string &str)
-{
-    const std::size_t found_ws = str.find_last_not_of(" ");
-
-    if (found_ws == std::string::npos) {
-        return;
-    }
-
-    str.erase(found_ws + 1);
-}
-
-void parse_configs(const std::string &file_contents, std::map<std::string, std::string> &raw_configs)
-{
-    std::istringstream is_file(file_contents);
-    std::string line;
-
-    while (std::getline(is_file, line)) // we read line by line - up to \n in this case
-    {
-        std::istringstream is_line(line);
-
-        std::size_t is_comment = line.find("#");
-        if (is_comment != std::string::npos) {
-            continue;
-        }
-
-        std::string key;
-
-        // then for the extracted line, is_line, we read from the beginning of the line to =
-        if (std::getline(is_line, key, '=')) {
-            strip_whitespace_from_left(key);
-            strip_whitespace_from_right(key);
-
-            std::string value;
-
-            // then we read between = and \n
-            if (std::getline(is_line, value)) {
-                strip_whitespace_from_left(value);
-                strip_whitespace_from_right(value);
-
-                raw_configs[key] = value;
-            }
+    const auto &targets = table["targets"];
+    if (targets.is_array()) {
+        for (auto it = targets.as_array()->begin(); it < targets.as_array()->end(); ++it) {
+            configs.commands.push_back(it->as_string()->get());
         }
     }
 }
@@ -120,46 +82,16 @@ void is_config_file_input_sane(const Configs &configs)
 
 Configs read_configs_from_file()
 {
-    std::string file_contents;
-
-    if (not utils::read_file(PROG_CONFIG, file_contents)) {
-        throw std::runtime_error("Could not load configurations. Cannot continue");
-    }
-
-    std::map<std::string, std::string> raw_configs;
-    parse_configs(file_contents, raw_configs);
-
-    logger::info("Parsed raw configs: ");
-    logger::debug_map(raw_configs);
-
     Configs configs;
 
-    for (auto it = raw_configs.begin(); it != raw_configs.end(); it++) {
-        if (it->first == "time-wake-hour") {
-            configs.time_wake.tm_hour = atoi(it->second.c_str());
-        } else if (it->first == "time-wake-minute") {
-            configs.time_wake.tm_min = atoi(it->second.c_str());
-        } else if (it->first == "time-cmd-hour") {
-            configs.time_run_cmd.tm_hour = atoi(it->second.c_str());
-        } else if (it->first == "time-cmd-minute") {
-            configs.time_run_cmd.tm_min = atoi(it->second.c_str());
-        } else if (it->first == "time-sleep-hour") {
-            configs.time_sleep.tm_hour = atoi(it->second.c_str());
-        } else if (it->first == "time-sleep-minute") {
-            configs.time_sleep.tm_min = atoi(it->second.c_str());
-        } else if (it->first == "suspend-type") {
-            configs.suspend_type = it->second.c_str();
-        } else if (it->first.find("target_") != std::string::npos) {
-            std::pair<std::string, std::string> command;
-            command.first = it->first;
-            command.second = it->second;
-            configs.commands.push_back(command);
-        } else {
-            logger::warning("Found unknown entry in config file: \"" + it->first + "\"");
-        }
+    try {
+        read_project_toml(configs);
+    } catch (const toml::parse_error &e) {
+        throw std::runtime_error(e.what());
+    } catch (const std::out_of_range &e) {
+        throw std::runtime_error(e.what());
     }
 
     is_config_file_input_sane(configs);
-
     return configs;
 }
