3386c6fc29d114a07d948d2bc22ebdd98f9150b4
dsw7 <dds3443@hotmail.com>
2025-05-09 20:35:53 -0700

    Move struct

--- Horologion/src/command_base.hpp
+++ Horologion/src/command_base.hpp
@@ -1,17 +1,6 @@
 #pragma once
 
-#include <ctime>
-#include <string>
-#include <vector>
-
-struct Configs {
-    std::vector<std::pair<std::string, std::string>> commands = {};
-    std::string suspend_type = "mem";
-
-    struct tm time_wake = { 0 };
-    struct tm time_run_cmd = { 0 };
-    struct tm time_sleep = { 0 };
-};
+#include "parse_config_file.hpp"
 
 class CommandBase {
 public:
--- Horologion/src/parse_config_file.hpp
+++ Horologion/src/parse_config_file.hpp
@@ -1,6 +1,18 @@
 #pragma once
 
+#include <ctime>
 #include <map>
 #include <string>
+#include <utility>
+#include <vector>
+
+struct Configs {
+    std::vector<std::pair<std::string, std::string>> commands = {};
+    std::string suspend_type = "mem";
+
+    struct tm time_wake = { 0 };
+    struct tm time_run_cmd = { 0 };
+    struct tm time_sleep = { 0 };
+};
 
 void parse_configs(const std::string &file_contents, std::map<std::string, std::string> &raw_configs);
